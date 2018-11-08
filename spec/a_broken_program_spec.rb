880926e6cc8174b022f23f54252526186e737dd0
Tara E. Sabella <tarasabella@gmail.com>
2018-11-08 15:41:29 +0000

    automatically backed up by learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'not to raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
-    }.to raise_error
+    }.to not raise_error
   end
 
 end
